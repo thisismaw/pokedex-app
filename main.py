fe37ef6e3d8542666d3be3849def95e80f74e359
thisismaw <iamwillempacardo@gmail.com>
2020-11-02 18:07:27 +0800

    code compeleted

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup as bs4
 pokeIcon = []  # okay
 pokeNum = []  # okay
 pokeName = []  # okay
-PokeFirstType = [] # okay
-PokeSecondType = [] # okay
+PokeFirstType = []  # okay
+PokeSecondType = []  # okay
 totalStats = []
 HP = []
 Atk = []
@@ -35,19 +35,44 @@ for item in pages:
             grabSpanIcon = str(getSpanIcon['data-src'])
             pokeIcon.append(grabSpanIcon)
         for name in findTr:
-            getName = name.find('a', class_='ent-name')
-            grabPokeName = getName.getText()
-            pokeName.append(grabPokeName)
-        for type in findTr:
-            getType = type.find('td', class_="cell-icon")
-            firstType = type.find('a', class_="type-icon")
+            # can be improved
+            getName = name.find('span', class_="img-fixed icon-pkmn")
+            getPokeName = str(getName['data-alt'])
+            finalGetName = getPokeName.replace('icon', '')
+            pokeName.append(finalGetName)
+        for gettype in findTr:
+            getType = gettype.find('td', class_="cell-icon")
+            firstType = gettype.find('a', class_="type-icon")
             getFirstType = firstType.getText()
             secondType = firstType.find_next_sibling('a')
             getSecondType = secondType.getText() if secondType else "None"
-            print(getFirstType + " " + getSecondType)
+            PokeFirstType.append(getFirstType)
+            PokeSecondType.append(getSecondType)
+        for stats in findTr:
+            grabTotalStats = stats.find('td', class_="cell-total")
+            getTotalStats = grabTotalStats.getText()
+            totalStats.append(getTotalStats)
+            grabHP = grabTotalStats.find_next_sibling('td')
+            getHP = grabHP.getText()
+            HP.append(getHP)
+            grabAtk = grabHP.find_next_sibling('td')
+            getAtk = grabAtk.getText()
+            Atk.append(getAtk)
+            grabDef = grabAtk.find_next_sibling('td')
+            getDef = grabDef.getText()
+            Defense.append(getDef)
+            grabSpAtk = grabDef.find_next_sibling('td')
+            getSpAtk = grabSpAtk.getText()
+            SpAtk.append(getSpAtk)
+            grabSpDef = grabSpAtk.find_next_sibling('td')
+            getSpDef = grabSpDef.getText()
+            SpDef.append(getSpDef)
+            grabSpd = grabSpDef.find_next_sibling('td')
+            getSpd = grabSpd.getText()
+            Speed.append(getSpd)
 
-
-
-# data = {"Number" : pokeNum , "Name" : pokeName , "Type" : pokeType , "Icon" : pokeIcon}
-# df = pd.DataFrame(data=data)
-# df.to_excel("D:/pokemonDB/pokemondb.xlsx", index=False)
+data = {"Icon": pokeIcon, "Number": pokeNum, "Name": pokeName, "First Type": PokeFirstType,
+        "Second Type": PokeSecondType, "Total Stats": totalStats,
+        "HP": HP, "Attack": Atk, "Defense": Defense, "Sp.Atk": SpAtk, "Sp.Def": SpDef, "Speed": Speed}
+df = pd.DataFrame(data=data)
+df.to_excel("D:/pokemonDB/pokemondb.xlsx", index=False)
